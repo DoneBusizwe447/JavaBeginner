c98846e74e260217aa8343006ad5abcbd60f5bce
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-10-21 10:52:14 +0200

    mpower

---
+++ Book2/Chapter1b2/Greeter.java
@@ -0,0 +1,12 @@
+package Chapter1b2;
+
+import javax.swing.JOptionPane; 
+public class Greeter
+{
+public void sayHello()
+ {
+ JOptionPane.showMessageDialog(null, 
+ "Hello, World!", "Greeter",
+ JOptionPane.INFORMATION_MESSAGE);
+ }
+}
