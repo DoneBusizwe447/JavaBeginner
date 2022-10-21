330024af2a4e4fd7c832d39081ee625307665765
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-10-21 19:09:43 +0200

    warpaint

---
+++ Book2/Chapter2b2/HelloApp.java
@@ -0,0 +1,11 @@
+package Chapter2b2;
+
+public class HelloApp
+{
+ public static void main(String[] args)
+ {
+ String helloMessage;
+ helloMessage = "Hello, World!";
+ System.out.println(helloMessage);
+ }
+}
