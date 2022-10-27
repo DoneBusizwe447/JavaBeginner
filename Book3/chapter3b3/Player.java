ac117f11fa947e691824ac526dd0197fce8b5b06
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-10-27 21:39:47 +0200

    swize

---
+++ Book3/chapter3b3/Player.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Player
+{
+ private int health;
+ public int getHealth()
+ {
+ return health;
+ }
+ public void setHealth(int h)
+ {
+ health = h;
+ }
+}
+class PrimeClass
+{
+ private Scanner sc = new Scanner(System.in);
+ public int x;
+ {
+ System.out.print(
+ "Enter the starting value for x: ");
+ x = sc.nextInt();
+ }
+}
