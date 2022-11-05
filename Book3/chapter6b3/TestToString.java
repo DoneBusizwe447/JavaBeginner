f601570424e365c6f48f73a6eeb970534994fa78
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-11-05 20:19:13 +0200

    78to

---
+++ Book3/chapter6b3/TestToString.java
@@ -0,0 +1,27 @@
+public class TestToString
+{
+ public static void main(String[] args)
+ {
+ Employee emp = new Employee("Martinez",
+ "Anthony");
+ System.out.println(emp.toString());
+ }
+}
+class Employee
+{
+ private String lastName;
+ private String firstName;
+ public Employee(String lastName, String firstName)
+ {
+ this.lastName = lastName;
+ this.firstName = firstName;
+ }
+
+    Employee(String martinez, String anthony) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Employee(String martinez, String anthony) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
