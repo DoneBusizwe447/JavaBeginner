e46a3d868dcba3a88c8a861da75fcf602ecb35a0
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-11-09 21:39:47 +0200

    hoursd

---
+++ Book4/chapter4b4/NewClass.java
@@ -0,0 +1,15 @@
+LinkedList<String> officers = new LinkedList<String>();
+officers.add("Blake");
+officers.add("Burns");
+officers.add("Houlihan");
+officers.add("Pierce");
+officers.add("McIntyre");
+for (String s : officers)
+ System.out.println(s);
+
+for (String s : officers)
+ System.out.println(s);
+
+officers.remove(3);
+
+
