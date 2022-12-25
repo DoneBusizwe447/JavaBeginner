eb329adfdd87c7afb0208133d64f538ad8a53885
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-12-25 21:33:58 +0200

    yessir

---
+++ Book6/chapter2b6/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter2b6;
+
+/**
+ *
+ * @author magni
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        private Button btn;
+        btn = new Button();
+btn.setText("Click me please!");
+    }
+    
+}
