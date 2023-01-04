f00099c3b48b2ac045ebe0f21b56d086051de958
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-04 21:48:53 +0200

    mandem

---
+++ Java4Dummies/Book3/chapter2b3/CountToTenError.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter4b2;
+
+/**
+ *
+ * @author magni
+ */
+public class CountToTenError {
+    public static void main(String[] args)
+    {
+        int i;
+        for (i = 1; i <=10; i++)
+            System.out.print(i);
+        System.out.println("The final value of i is " + 
+                i);
+    }
+    
+}
