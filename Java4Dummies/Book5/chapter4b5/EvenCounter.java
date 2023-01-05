b63ef7ed26e8d65b633ed381f04074dfaceabdb2
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-05 21:40:20 +0200

    yessir

---
+++ Java4Dummies/Book5/chapter4b5/EvenCounter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter4b2;
+
+/**
+ *
+ * @author magni
+ */
+public class EvenCounter {
+    public static void main(String[] args){
+        int number = 2;
+        while (number <= 20){
+            System.out.println( number +" ");
+            number += 2;
+        }               
+        System.out.println();
+    }                                        
+                                       
+}
