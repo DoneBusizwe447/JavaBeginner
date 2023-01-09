7e526e8685e3f7c499bbbd0fee132d246370c7d8
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-09 23:37:48 +0200

    deep

---
+++ Java4Dummies/Book2/Chapter3b2/Guessgame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guessingGame;
+
+/**
+ *
+ * @author magni
+ */
+public class Guessgame {
+    Player p1;
+    Player p2;
+    Player p3;
+    
+    public void startGame() {
+         Player  p1 = new Player();
+    }
+    
+    
+    
+    
+}
