299986ee8d5a3d2c1fe0da2a24b61b5b2517e3b6
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-09 23:38:21 +0200

    deep

---
+++ Java4Dummies/Book1/Chapter1b1/Java4DummiesAdvanced.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java4dummiesadvanced;
+
+/**
+ *
+ * @author magni
+ */
+public class Java4DummiesAdvanced {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
