dfcd37cfce3b88328d901fc3005e8061c0d62ca7
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-11 23:52:16 +0200

    hotshower

---
+++ Java4Dummies/Book5/ShoppingCart.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise;
+
+/**
+ *
+ * @author magni
+ */
+public class ShoppingCart {
+    public static void main (String[] args) {
+        
+    }
+    
+}
+
