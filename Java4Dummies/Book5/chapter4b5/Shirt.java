132e7e67bed523c0fdd3d34af5c9ae56e108d667
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-11 23:45:53 +0200

    hotshower

---
+++ Java4Dummies/Book5/chapter4b5/Shirt.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise;
+
+/**
+ *
+ * @author magni
+ */
+public class Shirt {
+    
+}
+   public void displayFields () {
+   Shirt shirt01 = new Shirt
+   publ
+} 
+
+public void setColor (String color) {
+if(color.length ()> 0)
+{
+ this.colorCode = color.charAt (index: 0);
+}
+}
