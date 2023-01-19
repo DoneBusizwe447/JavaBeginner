20a92f7b06b342711b4e17f056f653d55bd7e511
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-19 22:10:53 +0200

    manje

---
+++ Java4Dummies/Book5/DeckOfCards.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intro2javaprogram;
+
+/**
+ *
+ * @author magni
+ */
+public class DeckOfCards {
+    public static void main(String[] args) {
+        int[] deck = new int[52];
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack","Queen","King"};
+       
+       // initialized the cards
+       for(int i = 0; i < deck.length; i++)
+           deck[i] = i;
+       
+       //Shuffle the cards 
+      for (int i = 0; i < deck.length; i++) {
+          //xGenerate an index randomly
+          int index = (int)(Math.random()* deck.length);          
+          int temp = deck[i];
+          deck[i] = deck[index];
+          deck[index] = temp;
+    }
+      
+      for (int i = 0; i < 4; i++) {
+          String suit = suits[deck[i] / 13];
+          String rank = ranks[deck[i] % 13];
+          System.out.println("Card number " + deck[i] + ": " + rank + "of " + suit);
+      }
+      
+    }
+    
+}
