584c4ac679d96aab165e46198f5dc0607f496174
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-01-26 20:28:43 +0200

    g

---
+++ Java4Dummies/Book5/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter4b2;
+
+/**
+ *
+ * @author magni
+ */
+public class NewClass {
+    
+}
