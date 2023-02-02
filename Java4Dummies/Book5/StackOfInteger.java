6f513fdfe0b4da5b3fc45e24fd17972485a4b211
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-02-02 21:51:51 +0200

    uiu

---
+++ Java4Dummies/Book5/StackOfInteger.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intro2javaprogram;
+
+/**
+ *
+ * @author magni
+ */
+public class StackOfInteger {
+
+    public StackOfInteger() {
+    }
+
+    void push(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    boolean empty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String pop() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
